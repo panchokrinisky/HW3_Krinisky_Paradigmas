f9fd11b524d58db10d1d63fac37f9cb76bedcc1d
panchokrinisky <161512362+panchokrinisky@users.noreply.github.com>
2025-05-29 14:28:54 -0300

    Delete ej2 directory

--- ej2/source/Circulo.cpp
+++
@@ -1,9 +0,0 @@
-#include "Circulo.h"
-
-float Circulo::getX() const {return x;}
-float Circulo::getY() const {return y;}
-float Circulo::getRadio() const {return radio;}
-
-void Circulo::setX(float valor) {x = valor;}
-void Circulo::setY(float valor) {y = valor;}
-void Circulo::setRadio(float valor) {radio = valor;}
--- ej2/source/Punto.cpp
+++
@@ -1,7 +0,0 @@
-#include "Punto.h"
-
-float Punto::getX() const {return x;}
-float Punto::getY() const {return y;}
-
-void Punto::setX(float valor) {x = valor;}
-void Punto::setY(float valor) {y = valor;}     
